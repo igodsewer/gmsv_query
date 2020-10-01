4c475d8b09624ce8ba8588cf8192ae49b2068467
igodsewer <igodsewer1337@gmail.com>
2020-10-01 20:50:34 +0300

    1

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+newoption({
+	trigger = "gmcommon",
+	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
+	value = "path to garrysmod_common directory"
+})
+
+local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
+	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
+include(path.join(gmcommon, "generator.v2.lua"))
+
+CreateWorkspace({name = "serversecure.core", abi_compatible = true})
+	CreateProject({serverside = true})
+		IncludeLuaShared()
+		IncludeHelpersExtended()
+		IncludeSDKCommon()
+		IncludeSDKTier0()
+		IncludeSDKTier1()
+		IncludeSteamAPI()
+		IncludeDetouring()
+		IncludeScanning()
+		files({
+			"source/netfilter/*.cpp",
+			"source/netfilter/*.hpp"
+		})
